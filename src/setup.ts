1e2ed1a86bfe8784c8b5fb931a5ce0270409fac5
Kadugs <cadu20.gomes@gmail.com>
2021-12-20 14:58:32 -0300

    build heroku

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,13 @@
 import dotenv from 'dotenv';
 
-const path: string = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+let path: string = '.env';
+
+if (process.env.NODE_ENV === 'development') {
+  path = '.env.dev';
+}
+if (process.env.NODE_ENV === 'test') {
+  path = '.env.test';
+}
 
 dotenv.config({
   path,
