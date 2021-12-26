9776213cd56382a0378b40a4fe46554f57187985
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-26 01:28:00 -0300

    preparando para o review

--- src/routes/testsRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as testsController from '../controllers/testsController';
-
-const router = Router();
-
-router.post('/tests', testsController.addTest);
-
-export default router;
--- src/services/testsService.ts
+++
@@ -1,9 +0,0 @@
-import { getRepository } from 'typeorm';
-import TestInterface from '../interfaces/Test';
-import TestEntity from '../entities/TestEntity';
-
-async function addTest(testBody: TestInterface) {
-  await getRepository(TestEntity).insert(testBody);
-}
-
-export { addTest };
